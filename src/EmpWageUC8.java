2fbcc95c6c8fa1802e2734b9e8b405244bebf556
nisha <nishantsingh187@gmail.com>
2022-12-01 00:46:01 +0530

    Employee Wage UC8

---
+++ src/EmpWageUC8.java
@@ -0,0 +1,45 @@
+public class EmpWageUC8 {
+    static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME = 2;
+
+
+    static void calculateWage(String company, int wagePerHr, int daysPerMonth, int workHrPerMonth) {
+
+        int empHrs;
+        int totalHours = 0;
+        int dailyWage;
+        int day = 0;
+        int totalWage = 0;
+
+        while ((totalHours < workHrPerMonth) && (day < daysPerMonth)) {
+
+            int attendance = (int) (Math.floor(Math.random() * 10)) % 3;
+            switch (attendance) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            totalHours += empHrs;
+            dailyWage = wagePerHr * empHrs;
+            totalWage += dailyWage;
+            day++;
+            System.out.println("Day " + day + " Emp work hour " + empHrs + " wage is " + dailyWage);
+        }
+        System.out.println("Total work hours is : " + totalHours);
+        System.out.println("Total work days are: " + day);
+        //System.out.println("Total wage is: " + totalWage);
+        System.out.println("Total Wage of company " + company + "is" + totalWage);
+    }
+
+    public static void main(String[] args) {
+
+        calculateWage("TATA", 10, 20, 100);
+        calculateWage("APPLE", 20, 22, 90);
+    }
+}
